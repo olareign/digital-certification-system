72020991a290d211576e33e500d2afb5087497e6
Abdulrasaq Taofeeq Olarewaju <106169693+olareign@users.noreply.github.com>
2024-02-08 05:35:16 +0100

    Delete js src directory

--- src/middleware/not-found.js
+++
@@ -1,7 +0,0 @@
-const notFound = function (req, res, next){
-	res.status(404).send(
-		"Route not found"
-	)
-};
-
-module.exports = notFound
--- src/utils/generateResetPin.js
+++
@@ -1,7 +0,0 @@
-const generateResetPin = () => {
-    const minm = 100000;
-    const maxm = 999999;
-    return Math.floor(Math.random() * (maxm - minm + 1)) + minm;
-  }
-  
-  module.exports = generateResetPin
